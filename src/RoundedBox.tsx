732d0d11ca30f251d40db48dfe14385c947eaad3
Matt Gadd <mgadd@sitepen.com>
2019-12-05 00:11:24 +0000

    prettier

--- src/RoundedBox.tsx
+++ src/RoundedBox.tsx
@@ -4,6 +4,6 @@ const factory = create().properties();
 
 export default factory(function RoundedBox({ children }) {
 	return (
-		<div classes={'bg-white rounded-lg border border-gray-400 inline-block'}>{children()}</div>
+		<div classes="bg-white rounded-lg border border-gray-400 inline-block">{children()}</div>
 	);
 });
